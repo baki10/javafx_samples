5a54a4cff90f6fc057de0c37c2bf8bc70ba970ef
Ilmir <ilmirbakirov@gmail.com>
2017-03-03 15:38:16 +0500

    javafx shapes 3d

---
+++ src/com/bakigoal/shapes3d/CylinderExample.java
@@ -0,0 +1,38 @@
+package com.bakigoal.shapes3d;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Cylinder;
+import javafx.stage.Stage;
+
+public class CylinderExample extends Application {
+  @Override
+  public void start(Stage stage) {
+    //Drawing a Cylinder
+    Cylinder cylinder = new Cylinder();
+
+    //Setting the properties of the Cylinder
+    cylinder.setHeight(300.0f);
+    cylinder.setRadius(100.0f);
+
+    //Creating a Group object
+    Group root = new Group(cylinder);
+
+    //Creating a scene object
+    Scene scene = new Scene(root, 600, 300);
+
+    //Setting title to the Stage
+    stage.setTitle("Drawing a cylinder");
+
+    //Adding scene to the stage
+    stage.setScene(scene);
+
+    //Displaying the contents of the stage
+    stage.show();
+  }
+
+  public static void main(String args[]) {
+    launch(args);
+  }
+}
---
+++ src/com/bakigoal/shapes3d/SphereExample.java
@@ -0,0 +1,40 @@
+package com.bakigoal.shapes3d;
+
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.shape.Sphere;
+import javafx.stage.Stage;
+
+public class SphereExample extends Application {
+  @Override
+  public void start(Stage stage) {
+    //Drawing a Sphere
+    Sphere sphere = new Sphere();
+
+    //Setting the properties of the Sphere
+    sphere.setRadius(50.0);
+
+    sphere.setTranslateX(200);
+    sphere.setTranslateY(150);
+
+    //Creating a Group object
+    Group root = new Group(sphere);
+
+    //Creating a scene object
+    Scene scene = new Scene(root, 600, 300);
+
+    //Setting title to the Stage
+    stage.setTitle("Drawing a Sphere - draw fill");
+
+    //Adding scene to the stage
+    stage.setScene(scene);
+
+    //Displaying the contents of the stage
+    stage.show();
+  }
+
+  public static void main(String args[]) {
+    launch(args);
+  }
+}
